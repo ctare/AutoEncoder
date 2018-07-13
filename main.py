6e9e2a4b90acf0545d78d7415d982e612efb563a
ctare <sankaku900622@yahoo.co.jp>
2018-07-13 18:43:18 +0900

    double fc AE and test

--- main.py
+++ main.py
@@ -30,29 +30,30 @@ with tf.name_scope("optimize"):
     optimizer = tf.train.AdamOptimizer().minimize(loss)
 
 #%%
-# h2 = tf.contrib.slim.fully_connected(h, 100, activation_fn=tf.nn.sigmoid)
-# h2 = tf.contrib.slim.dropout(h2, 0.5)
-# output2 = tf.contrib.slim.fully_connected(h, 300, activation_fn=tf.nn.relu)
-#
-# with tf.name_scope("optimize2"):
-#     loss2 = tf.losses.mean_squared_error(output2, h)
-#     optimizer2 = tf.train.AdamOptimizer().minimize(loss2)
+with tf.variable_scope("dense2"):
+    h2 = tf.contrib.slim.fully_connected(h, 100, activation_fn=tf.nn.sigmoid)
+    h2_d = tf.contrib.slim.dropout(h2, 0.5)
+    output2 = tf.contrib.slim.fully_connected(h2_d, 300, activation_fn=tf.nn.relu)
+
+with tf.name_scope("optimize2"):
+    loss2 = tf.losses.mean_squared_error(output2, h)
+    optimizer2 = tf.train.AdamOptimizer().minimize(loss2, var_list=tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="dense2"))
 
 #%%
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 #%% test
-i = 3
+i = 10
 imgs = sess.run(output, feed_dict={input: np.expand_dims(input_data[i], 0)})
-pylab.subplot(2, 1, 1)
+pylab.subplot(1, 2, 1)
 pylab.imshow(np.reshape(imgs[0], (28, 28)))
 
-pylab.subplot(2, 1, 2)
+pylab.subplot(1, 2, 2)
 pylab.imshow(np.reshape(input_data[i], (28, 28)))
 pylab.show()
 
-#%% train
+#%% train 300
 batch_n = 4 ** 2
 ind = input_data[100:batch_n + 100]
 for i in range(300):
@@ -67,19 +68,33 @@ for i in range(300):
     pylab.imshow(to_plot_img(ind, batch_n, 28))
     pylab.show()
 
-#%%
+#%% train 100
+batch_n = 4 ** 2
+ind = input_data[100:batch_n + 100]
+for i in range(300):
+    _, hv, lossv = sess.run([optimizer2, output2, loss2], feed_dict={input: ind})
+    imgs = sess.run(output, feed_dict={h: hv})
+    pylab.subplot(1, 2, 1)
+    pylab.title("epoch: {}, loss: {:.2f}".format(i, lossv))
+    pylab.axis("off")
+    pylab.imshow(to_plot_img(imgs, batch_n, 28))
+    pylab.subplot(1, 2, 2)
+    pylab.axis("off")
+    pylab.imshow(to_plot_img(ind, batch_n, 28))
+    pylab.show()
 
-
-
-# batch_n = 64
-# for x in zip(*[iter(input_data)]* batch_n)[0]:
-#     sess.run(optimizer, feed_dict={input: x})
-
-# i = 0
-# imgs = sess.run(output, feed_dict={input: np.expand_dims(input_data[i], 0)})
-# pylab.subplot(2, 1, 1)
-# pylab.imshow(np.reshape(imgs[0], (28, 28)))
-#
-# pylab.subplot(2, 1, 2)
-# pylab.imshow(np.reshape(input_data[i], (28, 28)))
-# pylab.show()
+#%% test
+i = 10
+for i in range(100):
+    test_data = np.zeros((4, 100))
+    test_data[:, 6] = i / 100
+    test_data[:, 2] = i / 100
+    test_data[:, 10] = i / 100
+    hv = sess.run(output2, feed_dict={h2: test_data})
+    imgs = sess.run(output, feed_dict={h: hv})
+    # pylab.subplot(1, 2, 1)
+    pylab.imshow(to_plot_img(imgs, 4, 28))
+    #
+    # pylab.subplot(1, 2, 2)
+    # pylab.imshow(np.reshape(input_data[i], (28, 28)))
+    pylab.show()
